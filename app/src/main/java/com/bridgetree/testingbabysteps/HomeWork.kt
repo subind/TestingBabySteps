69f12a86f688cf1c6a16588c94c442cbf2a39fc9
subinds <subinds@bridgetree.com>
2020-10-23 10:13:45 +0530

    Business logic corrected

--- app/src/main/java/com/bridgetree/testingbabysteps/HomeWork.kt
+++ app/src/main/java/com/bridgetree/testingbabysteps/HomeWork.kt
@@ -10,13 +10,13 @@ object HomeWork {
      * fib(n) = fib(n-2) + fib(n-1)
      */
     fun fib(n: Int): Long {
-        if(n == 0 || n == 1) {
+        if (n == 0 || n == 1) {
             return n.toLong()
         }
         var a = 0L
         var b = 1L
         var c = 1L
-        (1..n-2).forEach { i ->
+        (1..n - 1).forEach { i ->
             c = a + b
             a = b
             b = c
@@ -29,7 +29,25 @@ object HomeWork {
      * e.g. "(a * b))" should return false
      */
     fun checkBraces(string: String): Boolean {
-        return string.count { it == '(' } == string.count { it == ')' }
+        var a = string.indexOf('(')
+        var b = string.indexOf(')')
+        var openCloseOrder = a < b
+
+        var countBalance = string.count { it == '(' } == string.count { it == ')' }
+
+        var x = string.indexOf('{')
+        var y = string.indexOf('[')
+        var z = string.indexOf('(')
+        var balancedBrackets = false
+        if(x.equals(-1)){
+            balancedBrackets = y < z
+        }else if(y.equals(-1)){
+            balancedBrackets = x < z
+        }else if(z.equals(-1)){
+            balancedBrackets = x < y
+        }
+
+        return openCloseOrder && countBalance && balancedBrackets
     }
 
 }
